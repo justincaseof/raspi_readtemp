1fbe11869c9ddaee6921cb3ba33ef57a82fe9a99
justin caseof <justin.caseof@gmail.com>
2018-12-09 15:44:10 +0100

    fixed missing import

--- database/database.go
+++ database/database.go
@@ -1,1 +1,41 @@
 package database
+
+import (
+	 "database/sql"
+	 "fmt"
+
+	 _ "github.com/lib/pq"
+)
+
+const (
+	host     = "192.168.171.34"
+	port     = 5432
+	user     = "pitemp"
+	password = "pitemp"
+	dbname   = "pitemp"
+)
+
+func InitDatabase() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	ensureTableExists("foobar123")
+}
+
+/**
+ tableIdentifier should be the raspi's mac address
+ */
+func ensureTableExists(tableIdentifier string) {
+
+}
--- readtemperature/readtemperature_linux.go
+++ readtemperature/readtemperature_linux.go
@@ -1,5 +1,7 @@
 package readtemperature
 
+import "os/exec"
+
 func ReadTemperatureExecutableCommand() *exec.Cmd {
 	return exec.Command("/opt/vc/bin/vcgencmd", "measure_temp")
 }
