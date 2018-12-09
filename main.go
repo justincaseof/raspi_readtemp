fc10886832efeb128c66d99558790bdb917beb53
justin caseof <justin.caseof@gmail.com>
2018-12-09 13:07:50 +0100

    tested go routines

--- main.go
+++ main.go
@@ -3,18 +3,28 @@ package main
 import (
 	"pitemp/logging"
 	"pitemp/readtemperature"
+	"sync"
 	"time"
 )
 import "go.uber.org/zap"
 
 var logger = logging.New("pitemp", false)
+var allJobsDone = &sync.WaitGroup{}
 
 func main(){
 	logger.Info("### STARTUP")
-	loopedTemperatureRead()
+
+	allJobsDone.Add(1)
+	go loopedTemperatureRead("go")
+
+	allJobsDone.Wait()
+	logger.Info("### EXIT")
 }
 
-func loopedTemperatureRead() {
+func loopedTemperatureRead(foo string) {
+	logger.Info("IN:  %s", zap.String("foo", foo))
+	defer allJobsDone.Done()
+
 	for {
 		info, err := readtemperature.GetTemp()
 		if err==nil {
@@ -22,5 +32,6 @@ func loopedTemperatureRead() {
 		}
 		time.Sleep(time.Second)
 	}
+	logger.Info("OUT: %s", zap.String("foo", foo))
 }
 
